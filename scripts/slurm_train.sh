568fa6e9bb942d8eb735ba003fa0a9a9c2015b6f
sombit888 <sombit.dey@insait.ai>
2024-09-06 19:28:01 +0000

    without aug config now

--- scripts/slurm_train.sh
+++ scripts/slurm_train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=original_training   # Job name
+#SBATCH --job-name=original_training_without_aug   # Job name
 #SBATCH --nodes=1                    # Number of nodes
 #SBATCH --cpus-per-gpu=12
 #SBATCH --nodelist=gcpl4-eu-1
 #SBATCH --gres=gpu:l4-24g:1    
 #SBATCH --time=48:00:00        
-#SBATCH --mem-per-gpu=32
+#SBATCH --mem-per-gpu=32G
 #SBATCH --output=/scratch/sombit_dey/job_%j.out
 #SBATCH --error=/scratch/sombit_dey/job_%j.err
 
@@ -15,8 +15,8 @@ export CUDA_HOME=/opt/modules/nvidia-cuda-11.8.0/
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 export PATH=$CUDA_HOME/bin:$PATH
 
-micromamba activate 3dvista
 
 cd /scratch/sombit_dey/3D-VisTA
+git pull origin main
 micromamba run -n 3dvista python run.py --config project/vista/nr3d_config.yml           
 # python run.py --config project/vista/nr3d_config.yml 
