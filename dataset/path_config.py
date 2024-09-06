7a8fdb55e17510b4053e672a12d236e3e88bf3d5
sombit888 <sombit.dey@insait.ai>
2024-09-06 19:00:12 +0000

    scratch reproduce

--- dataset/path_config.py
+++ dataset/path_config.py
@@ -1,6 +1,8 @@
 import os 
 # path fixed
-SCAN_FAMILY_BASE = "./data/scanfamily"
+# SCAN_FAMILY_BASE = "./data/scanfamily"
+SCAN_FAMILY_BASE = "/scratch/sombit_dey/referit3d"
+
 
 # path generated
 MASK_BASE = os.path.join(SCAN_FAMILY_BASE, "save_mask")
